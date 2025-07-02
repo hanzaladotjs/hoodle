da82e6ce2a65b1caea7a47b54087b68d2645efb2
hanzaladotjs <bigapplestorekmr@gmail.com>
2025-07-02 15:29:26 +0530

    ui theme changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,28 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
-import Blockchains from './components/Blockchains'
-import HotTokens from './components/HotTokens'
-import Prices from './components/Prices'
+import Home from './components/Home'
 import Sidebar from './components/Sidebar'
 
-import BackgroundDots from './components/BackgroundDots'
 function App() {
 
   return (
-    <div>
-    <BackgroundDots style={{ zIndex: 0} }
- />
+   
     <div className='flex flex-col'>
     
     <Navbar/>
     <div className='flex'>
       <Sidebar/>
-      <div className='w-full min-h-screen bg-white'>
+      <div className='w-full min-h-screen bg-white border-t text-[#ffafe2]'
+      >
         <Routes>
-          <Route path='/' element={<Blockchains/>}></Route>
-          <Route path ='/price' element={<Prices/>}></Route>
-          <Route path='/trend' element={<HotTokens/>}> </Route>
+          <Route path='/' element={<Home/>}></Route>
+         
         </Routes>
       </div>
     </div>
    </div>
-   </div>
+ 
   )
 }
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+const Home = () =>{
+    return (
+        <div className="flex flex-col justify-center min-h-250 space-y-20 items-center text-black ">
+            <h1 className="text-8xl font-semibold hover:text-[#b63788] mr-55">   Welcome to Hoodle </h1> 
+            <p className="text-5xl w-300 hover:text-blue-800"> Explore different blockchain ecosystems, cryptocurrencies, stable coins, exchanges, and much more.
+            </p>
+          
+        </div>
+    )
+}
+
+export default Home
