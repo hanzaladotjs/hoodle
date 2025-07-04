0d41d5e32c59f56a57111f1e71a6592b09889674
hanzaladotjs <bigapplestorekmr@gmail.com>
2025-07-04 11:46:04 +0530

    hopefully

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,15 +3,15 @@ const Sidebar = () => {
     return (
 
 
-        <div className="min-h-screen bg-blue-200 border-r text-black w-120  md:flex flex-col font-bold hidden ">
+        <div className="min-h-25000 bg-yellow-100 z-50 border-r text-black w-110 md:flex flex-col font-bold hidden ">
           
-            <div className="flex flex-col fixed text-2xl text-center justify center w-100">
-               <Link to={"/"}> <h2 className="py-6 hover:text-3xl text-center">Home</h2> </Link>
-               <Link to={"/trend"} ><h2 className="py-6   hover:text-3xl text-center">Top Coins</h2></Link>
-               <Link to ="/ex" ><h2 className="py-6  hover:text-3xl text-center">Explore Exchnages</h2></Link>
+            <div className="flex flex-col fixed text-2xl text-center justify-center w-100">
+               <Link to={"/"}> <h2 className="py-6 hover:text-3xl hover:border-2 w-110 text-center hover:bg-blue-200">Home</h2> </Link>
+               <Link to={"/trend"} ><h2 className="py-6  w-110 hover:border-2   hover:text-3xl text-center hover:bg-yellow-200">Top Coins</h2></Link>
+               <Link to ="/ex" ><h2 className="py-6 w-110 hover:border-2  hover:text-3xl text-center hover:bg-green-200">Explore Exchnages</h2></Link>
                 
-               <Link to={"/global"}><h2 className="py-6 hover:text-3xl text-center  ">Global Crypto Market</h2> </Link>
-               <Link to={"/chains"} ><h2 className="py-6 hover:text-3xl text-center  ">Explore Blockchains</h2></Link>
+               <Link to={"/global"}><h2 className="py-6 w-110 hover:border-2  hover:text-3xl text-center hover:bg-orange-200 ">Global Crypto Market</h2> </Link>
+               <Link to={"/chains"} ><h2 className="py-6  w-110 hover:border-2  hover:text-3xl text-center  hover:bg-pink-200 ">Explore Blockchains</h2></Link>
             </div>
             
         </div>
