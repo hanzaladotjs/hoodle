81ea47c1f475ff4d6e96a19c80d6aa2bcbc0b04a
hanzaladotjs <bigapplestorekmr@gmail.com>
2025-07-05 21:10:11 +0530

    sidebar made sticky

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,9 +3,9 @@ const Sidebar = () => {
     return (
 
 
-        <div className="min-h-25000 bg-yellow-100 z-50 border-r text-black w-110 md:flex flex-col font-bold hidden ">
+        <div className="min-h-25000 bg-yellow-100 z-50 border-r text-black w-110 md:flex flex-col font-bold hidden absolute top-25 left-0">
           
-            <div className="flex flex-col fixed text-2xl text-center justify-center w-100">
+            <div className="flex flex-col text-2xl text-center justify-center w-100 sticky top-25 left-0">
                <Link to={"/"}> <h2 className="py-6 hover:text-3xl hover:border-2 w-110 text-center hover:bg-blue-200">Home</h2> </Link>
                <Link to={"/trend"} ><h2 className="py-6  w-110 hover:border-2   hover:text-3xl text-center hover:bg-yellow-200">Top Coins</h2></Link>
                <Link to ="/ex" ><h2 className="py-6 w-110 hover:border-2  hover:text-3xl text-center hover:bg-green-200">Explore Exchnages</h2></Link>
